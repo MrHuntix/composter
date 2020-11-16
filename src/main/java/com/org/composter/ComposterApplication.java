45317310f2a12bae97ab43fa70067361aae5e9e7
Puneeth <puntirock717@gmail.com>
2020-11-16 11:36:01 +0530

    initial setup

---
+++ src/main/java/com/org/composter/ComposterApplication.java
@@ -0,0 +1,13 @@
+package com.org.composter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComposterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComposterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/composter/ComposterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.composter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComposterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
